436286fa8ec5384454d165d062b10b3734ba3587
manish sharma <msmanish407@gmail.com>
2026-04-30 19:52:37 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,17 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import Dashboard from "./pages/dashboard";
+import { BookOpen, Home } from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+import SubjectsList from "./pages/Subjects/list";
+import SubjectsCreate from "./pages/Subjects/create";
 
 function App() {
   return (
@@ -29,9 +34,28 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "1p0aZ4-jL82ae-yiyz6f",
               }}
+              resources={[
+                {
+                name: 'dashboard', list: '/', meta: { label: 'Home', icon: <Home/> }
+              },
+              {
+                name:'subjects',
+                list:'/subjects',
+                create:'/subjects/create',
+                meta:{label:'Subjects', icon:<BookOpen/>},
+              }
+            ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                                <Layout>
+                                  <Outlet /> 
+                                </Layout>}>
+                  <Route path="/" element={<Dashboard/>} />
+                  
+                  <Route index element={<SubjectsList/>} />
+                  <Route path="create" element={<SubjectsCreate/>} />
+                  </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
