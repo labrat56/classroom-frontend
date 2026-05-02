0a448b9d6bf0a91ca2a7f05257e0b3a6dcc40191
manish sharma <msmanish407@gmail.com>
2026-05-02 12:54:20 +0530

    Fixed errors

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,48 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+import { BACKEND_BASE_URL } from "@/Constants";
+import { ListResponse } from "@/pages/Subjects/types";
+import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
+
+if(!BACKEND_BASE_URL)
+  throw new Error('Please add BACKEND_BASE_URL')
+
+
+const options: CreateDataProviderOptions = {
+  getList: {
+    getEndpoint: ({ resource }) => resource,
+
+    buildQueryParams: async({resource, pagination, filters}) => {
+      const page = pagination?.currentPage ?? 1;
+      const pageSize = pagination?.pageSize ?? 1;
+
+      const params: Record<string, string|number> = {page, limit: pageSize};
+
+      filters?.forEach((filter) => {
+        const field = 'field' in filter ? filter.field : '';
+
+        const value = String(filter.value);
+
+        if(resource === 'subjects') {
+          if(field === 'department') params.department = value;
+          if(field === 'name' || field === 'code') params.search = value;
+
+        }
+      })
+
+      return params;
+    },
+
+    mapResponse: async (response) => {
+      const payload: ListResponse = await response.clone().json();
+      return payload.data ?? [];
+    },
+
+    getTotalCount: async (response) => {
+      const payload: ListResponse = await response.clone().json();
+      return payload.pagination?.total ?? payload.data?.length ?? 0;
+    }
+  }
+}
+
+const { dataProvider } = createDataProvider(BACKEND_BASE_URL, options);
+
+export { dataProvider };
